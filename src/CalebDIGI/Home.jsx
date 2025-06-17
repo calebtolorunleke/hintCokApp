6f648624efc99db9d8dd607d8303eba70c2326c6
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-06-17 00:11:44 -0700

    time zone change

--- src/CalebDIGI/Home.jsx
+++ src/CalebDIGI/Home.jsx
@@ -5,7 +5,7 @@ import Canada from "./Canada";
 const Home = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-900 to-gray-900 text-white flex items-center justify-center px-4">
-      <div className="flex flex-col md:flex-row gap-10 w-full max-w-5xl">
+      <div className="flex flex-col md:flex-row gap-5 w-full max-w-5xl">
         <Nigeria />
         <Canada />
       </div>
